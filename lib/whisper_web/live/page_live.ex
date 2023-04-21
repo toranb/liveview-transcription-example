0b07e41baa40a2c57c56df4b206c36c1d3e3d900
toran billups <toranb@gmail.com>
2023-04-21 12:11:52 -0500

    move audio task inline with page live

--- lib/whisper_web/live/page_live.ex
+++ lib/whisper_web/live/page_live.ex
@@ -59,7 +59,7 @@ defmodule WhisperWeb.PageLive do
     {:ok, %{duration: duration}} = Mp3Duration.parse(path)
 
     task =
-      Whisper.Audio.speech_to_text(duration, path, 20, fn ss, text ->
+      speech_to_text(duration, path, 20, fn ss, text ->
         {duration, ss, text}
       end)
 
@@ -67,6 +67,33 @@ defmodule WhisperWeb.PageLive do
   end
 
   def handle_progress(_name, _entry, socket), do: {:noreply, socket}
+
+  def speech_to_text(duration, path, chunk_time, func) do
+    Task.async(fn ->
+      format = get_format()
+
+      0..duration//chunk_time
+      |> Task.async_stream(
+        fn ss ->
+          args = ~w(-ac 1 -ar 16k -f #{format} -ss #{ss} -t #{chunk_time} -v quiet -)
+          {data, 0} = System.cmd("ffmpeg", ["-i", path] ++ args)
+          {ss, Nx.Serving.batched_run(WhisperServing, Nx.from_binary(data, :f32))}
+        end,
+        max_concurrency: 4,
+        timeout: :infinity
+      )
+      |> Enum.map(fn {:ok, {ss, %{results: [%{text: text}]}}} ->
+        func.(ss, text)
+      end)
+    end)
+  end
+
+  def get_format() do
+    case System.endianness() do
+      :little -> "f32le"
+      :big -> "f32be"
+    end
+  end
 
   @impl true
   def render(assigns) do
